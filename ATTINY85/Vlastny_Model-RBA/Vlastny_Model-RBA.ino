4ec92253c6e2b1df2d39a23a6405a1e626a53735
Galeje <galejenextgen@gmail.com>
2017-05-11 15:47:09 +0200

    basic funktionality

---
+++ ATTINY85/Vlastny_Model-RBA/Vlastny_Model-RBA.ino
@@ -0,0 +1,12 @@
+#include "Attiny85_IO.h"
+Attiny attiny;
+void setup() {
+
+
+}
+
+void loop() 
+{
+  if (attiny.LightSensor(1,"digital")==1){attiny.motor ("A", 1, "digital");}
+  else {attiny.motor("A", 0, "digital");}
+}
