8354487d225e74b6c2eb250e499d699bce5f61e5
Galeje <galejenextgen@gmail.com>
2017-05-12 14:34:14 +0200

    Temperature sensor update

--- Cing.ino
+++ Cing.ino
@@ -7,6 +7,6 @@ void setup() {
 
 void loop() 
 {
-  if (attiny.LightSensor(1,"digital")==1){attiny.motor ("A", 1, "digital");}
+  if (attiny.Temp(0)>30){attiny.motor ("A", 1, "digital");}
   else {attiny.motor("A", 0, "digital");}
 }
