cc73ba61e29d9d6df501a02fbc45d6488637ffa9
Galeje <galejenextgen@gmail.com>
2017-06-05 07:33:39 +0200

    Rename _7kapitolaponovm.ino to kniha/_7kapitolaponovm.ino

---
+++ kniha/_7kapitolaponovm.ino
@@ -0,0 +1,27 @@
+#include "Attiny85_IO_basic.h"
+Attiny attiny;
+#dafine ciara 0
+void setup() {
+
+
+}
+
+void loop() 
+{
+ 
+if (attiny.ShineSensor()<20)
+   {
+    attiny.motor("AB", 0, "digital");
+   }
+else 
+   {
+    if (attiny.LightSensor(2, "digital")==ciara) 
+      {
+        attiny.motor("B", 1, "digital");
+      }
+    else 
+      {
+        attiny.motor("A", 1, "digital");
+      } 
+   }
+}
