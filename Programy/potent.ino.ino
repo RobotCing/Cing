c2c30b770ab39dc18ac618ad5e40428a1045a657
GalejeNextGen <galejenextgen@gmail.com>
2017-09-24 21:47:25 +0200

    Add files via upload

---
+++ Programy/potent.ino.ino
@@ -0,0 +1,18 @@
+#include "Attiny85_IO_basic.h"
+Attiny attiny;
+void setup() {
+
+}
+
+void loop() 
+{
+  if (attiny.PotentiometerExternal()>50)
+    {
+          attiny.motor("AB", 1, "digital");              
+    }  
+ 
+  else
+    {
+      attiny.motor("AB", 0, "digital");
+    }
+}
