4de003055259dcd8ff54543fda27ace28c4aa0f1
GalejeNextGen <galejenextgen@gmail.com>
2017-10-12 08:35:28 +0200

    Kapitola: Prekážky

--- Programy/08_Nastavujeme hodnotu.ino
+++ Programy/08_Nastavujeme hodnotu.ino
@@ -1,16 +1,12 @@
 #include "Attiny85_IO_basic.h"
 Attiny attiny;
-void setup() {
-
-}
-
+void setup() {}
 void loop() 
 {
   if (attiny.PotentiometerExternal()>50)
     {
           attiny.motor("AB", 1, "digital");              
     }  
- 
   else
     {
       attiny.motor("AB", 0, "digital");
