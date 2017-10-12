a363a0a13b2538ce331cd27d2ca61317cd7cf5f1
GalejeNextGen <galejenextgen@gmail.com>
2017-10-12 14:08:05 +0200

    Kapitola: Osvetlenie

--- Programy/007_Osvetlenie.ino
+++ Programy/007_Osvetlenie.ino
@@ -1,14 +1,9 @@
 #include "Attiny85_IO_basic.h"
 Attiny attiny;
 #dafine ciara 0
-void setup() {
-
-
-}
-
+void setup() {}
 void loop() 
 {
- 
 if (attiny.ShineSensor()<20)
    {
     attiny.motor("AB", 0, "digital");
