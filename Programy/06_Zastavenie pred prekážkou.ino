d7c04c387da02e67b7ac15709700a346582b3bb8
GalejeNextGen <galejenextgen@gmail.com>
2017-10-12 08:30:43 +0200

    Kapitola: Prekážky

--- Programy/06_Zastavenie pred prekážkou.ino
+++ Programy/06_Zastavenie pred prekážkou.ino
@@ -1,9 +1,6 @@
 #include "Attiny85_IO.h"
 Attiny attiny;
-void setup() {
-
-
-}
+void setup() {}
 void loop() 
   {
     if (attiny.UltrasonicSensor()<20) 
@@ -15,6 +12,3 @@ void loop()
       attiny.motor("AB",1, "digital");
     }
   }
-
-
-
