d75cfce5893d59f34fc075ca1078b361e6dbc6fc
RobotCing <robotcing@gmail.com>
2018-07-22 15:43:10 +0200

    Add files via upload

---
+++ Examples/library_example/library_example.ino
@@ -0,0 +1,27 @@
+/*
+Program for line following using Cing Robot library
+*/
+//Including basic library for Cing robot with Attiny85 procesor
+#include <Attiny85_IO_basic.h>
+//Constructor
+Attiny attiny;
+void setup() {}
+void loop() 
+  {
+    //Read value from sensor 2
+    if (attiny.LightSensor(2,"digital")==1) 
+      {
+        //Turn of motor A
+        attiny.Runmotor("A", 0, "digital");
+        //Turn on motor B
+        attiny.Runmotor("B", 1, "digital");
+      }
+    else 
+      {
+        //Turn of motor B
+        attiny.Runmotor("B", 0, "digital");
+        //Turn on motor A
+        attiny.Runmotor("A", 1, "digital");
+      }
+}
+
